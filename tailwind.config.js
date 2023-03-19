c86fd95d1adf21fb601336e4689b41a0655c4a1b
MaxChuloshnikov <Maks447@ukr.net>
2023-03-19 18:33:20 +0200

    Header Done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const {fontFamily} = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -9,7 +11,34 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    maxWidth: {
+      container: "1520px",
+      contentContainer: "1280px",
+    },
+    extend: {
+      xs: "320px",
+      sm: "375px",
+      sml: "500px",
+      md: "667px",
+      mdl: "768px",
+      lg: "960px",
+      lgl: "1024px",
+      xl: "1280px",
+      "2x1": "1400px",
+    },
+    colors: {
+      blue: "#0071dc",
+      lightBlue: "#e6f1fc",
+      yellow: "#ffc220",
+      hoverBg: "#004f9a",
+      lightText: "#46474a",
+    },
+    boxShadow: {
+      bannerShadow: "0 1px 2px 1px #00000026"
+    },
+    fontFamily: {
+      sans: ["var(--font-open_sans)", ...fontFamily.sans],
+    },
   },
   plugins: [],
 }
